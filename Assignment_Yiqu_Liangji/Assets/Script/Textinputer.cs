a4cfd9368bd620176515cb39962295aa482c2b7d
Liangji Ma <15703476199ma@gmail.com>
2020-10-18 22:38:49 -0400

    Liangji Ma: add NPC

---
+++ Assignment_Yiqu_Liangji/Assets/Script/Textinputer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Textinputer : MonoBehaviour
+{
+    public GameObject Textbox;
+    public Text thetext;
+    public TextAsset textFile;
+    public string[] textLines;
+    public int currentline;
+    public int endline;
+    public GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        if(textFile!=null)
+        textLines=(textFile. text. Split('\n'));    
+    }
+    void Update()
+    {
+        thetext.text = textLines[currentline];
+        if(Input.GetMouseButtonDown(0))
+        {
+            currentline +=1;
+        }
+        if(currentline>endline){
+            Textbox.SetActive(false);
+        }
+    }
+}
