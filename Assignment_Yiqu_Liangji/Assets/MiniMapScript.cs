b0b66766189d3a0560806f47720af0207ecd0ea8
Elon Zhang <clearlovezyq@gmail.com>
2020-10-19 20:32:52 -0400

    Yiqu Zhang: MiniMap add hotkey to open or close.

---
+++ Assignment_Yiqu_Liangji/Assets/MiniMapScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapScript : MonoBehaviour
+{
+
+
+    public GameObject miniMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        miniMap.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.N)) {
+            Debug.Log("input get");
+            miniMap.SetActive(!CheckStatus());
+        }
+    }
+
+    private bool CheckStatus() {
+
+        if (miniMap.activeInHierarchy) {
+            return true;
+        }
+        return false;
+    }
+}
