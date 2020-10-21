946deeef0be39202e4ef6aec360e413485c5c953
Liangji Ma <15703476199ma@gmail.com>
2020-10-20 21:22:04 -0400

    Liangji Ma: Cam Fixed

--- Assignment_Yiqu_Liangji/Assets/CamFellow.cs
+++ Assignment_Yiqu_Liangji/Assets/CamFellow.cs
@@ -1,20 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class CamFellow : MonoBehaviour
 {
-    private Vector3 offset;
-    public Transform player;
- // Start is called before the first frame update
+    public Transform target;
+    Vector3 offset;
+    // Use this for initialization
     void Start()
     {
-        offset = player.position - transform.position;
+        offset = transform.position - target.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-         transform.position = Vector3.Lerp(transform.position, player.position - offset,Time.deltaTime*5);
+        transform.position = target.position + offset;
+        Rotate();
+        Scale();
+    }
+    private void Scale()
+    {
+        float dis = offset.magnitude;
+        dis += Input.GetAxis("Mouse ScrollWheel") * 5;
+        if (dis < 10 || dis > 40)
+        {
+            return;
+        }
+        offset = offset.normalized * dis;
+    }
+    private void Rotate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 pos = transform.position;
+            Vector3 rot = transform.eulerAngles;
+
+            transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X") * 20);
+            transform.RotateAround(target.position, Vector3.left, Input.GetAxis("Mouse Y") * 20);
+            float x = transform.eulerAngles.x;
+            float y = transform.eulerAngles.y;
+
+            if (x < 20 || x > 45 || y < 0 || y > 40)
+            {
+                transform.position = pos;
+                transform.eulerAngles = rot;
+            }
+            offset = transform.position - target.position;
+        }
     }
 }
---
+++ Assignment_Yiqu_Liangji/Assets/minimapcam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minimapcam : MonoBehaviour
+{
+    public Transform Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = Player.transform.position.x;    
+        float posZ = Player.transform.position.z;    
+        float OffsetY = Player.transform.position.y + 20;  
+        Vector3 pos = new Vector3(posX, OffsetY, posZ);
+        transform.position = pos;
+    }
+}
